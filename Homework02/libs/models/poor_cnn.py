fa5909358be6e666c174ea8a0af99b940e89c97a
Viktoriia Vlasenko <48601991+vikavl@users.noreply.github.com>
2024-11-24 03:43:19 +0100

    change poor performing cnn for q4

--- Homework02/libs/models/poor_cnn.py
+++ Homework02/libs/models/poor_cnn.py
@@ -3,7 +3,7 @@ import torch
 
 class PoorPerformingCNN(nn.Module):
     def __init__(self):
-        super(PoorPerformingCNN, self).__init__()
+        super().__init__()
         ##############################
         ###     CHANGE THIS CODE   ###
         ##############################  
@@ -11,13 +11,20 @@ class PoorPerformingCNN(nn.Module):
         self.relu1 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.conv2 = nn.Conv2d(5, 8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
 
-        self.fc1 = nn.Linear(8 * 4 * 4, 28)
+        self.fc1 = nn.Linear(8 * 8 * 8, 28)
 
     def forward(self, x):
+        # 32x32 color image: 3 channels RGB
+        # input: 32x32x3
+        # after conv1: 32x32x4
+        # after max pooling: 16x16x4 
         x = self.pool(self.relu1(self.conv1(x)))
+        # input: 16x16x4 
+        # after conv2: 16x16x8
+        # after max pooling: 8x8x8
         x = self.pool(self.relu2(self.conv2(x)))
         x = x.view(-1, 8 * 8 * 8)
         x = self.fc1(x)
